67a2bd99cb852f4eeebb7554b6a7fd8454454c3f
Jamu Kakar <jkakar@kakar.ca>
2015-04-03 20:24:11 -0700

    Add a test for AWS_REGION handling in default_config/0.

--- src/aws_config.erl
+++ src/aws_config.erl
@@ -33,6 +33,7 @@ default_region(Region) -> Region.
 -ifdef(TEST).
 -include_lib("eunit/include/eunit.hrl").
 
+%% default_config/0 returns a configuration map with default values.
 default_config_test() ->
     ?assertEqual(#{endpoint => undefined,
                    region => undefined,
@@ -40,5 +41,27 @@ default_config_test() ->
                    secret_access_key => undefined,
                    session_token => undefined},
                  default_config()).
+
+%% default_config/0 initializes the region from the AWS_REGION environment
+%% variables when it's set.
+default_config_with_region_environment_variable_test_() ->
+    AWSRegion = os:getenv("AWS_REGION"),
+    {spawn,
+     {setup,
+      fun () -> os:putenv("AWS_REGION", "us-east-1-a") end,
+      fun (_) -> case AWSRegion of
+                     false -> os:unsetenv("AWS_REGION");
+                     _ -> os:putenv("AWS_REGION", AWSRegion)
+                 end
+      end,
+      [fun () -> ?assertEqual(#{endpoint => undefined,
+                                region => "us-east-1-a",
+                                access_key_id => undefined,
+                                secret_access_key => undefined,
+                                session_token => undefined},
+                              default_config())
+       end]
+     }
+    }.
 
 -endif.
